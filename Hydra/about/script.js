8b1d66bc25055b22badff0fc18e5fd8656c27c09
uRanDoma <pogrebniyrusla@gmail.com>
2025-11-14 02:14:02 +0200

    Add files via upload

---
+++ Hydra/about/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // --- Модальне вікно ---
+  const modal = document.getElementById("modal");
+  const openBtn = document.getElementById("openModalBtn");
+  const closeBtn = document.querySelector(".close");
+
+  if (openBtn && modal && closeBtn) {
+    openBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.style.display = "flex";
+    });
+
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  }
+
+});
